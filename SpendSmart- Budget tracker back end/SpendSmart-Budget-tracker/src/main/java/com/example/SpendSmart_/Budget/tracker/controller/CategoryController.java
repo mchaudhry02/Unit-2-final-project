4b338079fff74957c9e4280b5e454fd3fb751d83
mchaudhry02 <mchaudhry20@hotmail.com>
2026-06-28 18:52:38 -0500

    update the cross origin to the correct local host like the front end local host

--- SpendSmart- Budget tracker back end/SpendSmart-Budget-tracker/src/main/java/com/example/SpendSmart_/Budget/tracker/controller/CategoryController.java
+++ SpendSmart- Budget tracker back end/SpendSmart-Budget-tracker/src/main/java/com/example/SpendSmart_/Budget/tracker/controller/CategoryController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:5173")
 public class CategoryController {
 
     @Autowired
--- SpendSmart- Budget tracker back end/SpendSmart-Budget-tracker/src/main/java/com/example/SpendSmart_/Budget/tracker/controller/TransactionController.java
+++ SpendSmart- Budget tracker back end/SpendSmart-Budget-tracker/src/main/java/com/example/SpendSmart_/Budget/tracker/controller/TransactionController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/transactions")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:5173")
 public class TransactionController {
 
     @Autowired
