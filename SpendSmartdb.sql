7aebddc8a35081c2a5988f03abd13b890047c45a
mchaudhry02 <mchaudhry20@hotmail.com>
2026-07-01 14:00:10 -0500

    Spend smart sql query script

---
+++ SpendSmartdb.sql
@@ -0,0 +1,40 @@
+SELECT * FROM categories;
+SELECT * FROM transactions;
+SELECT * FROM transaction_details;
+USE budget_tracker;
+SHOW TABLES;
+
+
+SELECT 
+  t.id,
+  t.description,
+  t.amount,
+  t.type,
+  t.date,
+  c.name AS category_name
+FROM transactions t
+LEFT JOIN categories c ON t.category_id = c.id;
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM transactions;
+SET SQL_SAFE_UPDATES = 1;
+
+SELECT 
+  t.id,
+  t.description,
+  t.amount,
+  t.type,
+  t.date,
+  c.name AS category_name
+FROM transactions t
+LEFT JOIN categories c ON t.category_id = c.id;
+
+CREATE VIEW transaction_details AS
+SELECT 
+  t.id,
+  t.description,
+  t.amount,
+  t.type,
+  t.date,
+  c.name AS category_name
+FROM transactions t
+LEFT JOIN categories c ON t.category_id = c.id;
